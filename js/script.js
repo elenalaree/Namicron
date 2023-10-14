9b14c9d23be03750bab2c0f4307f99cf6cc0e75d
Elena Carey <theelenacarey@gmail.com>
2023-10-13 22:23:44 -0500

    Got the first portion of names working in console.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,86 @@
 // Collect the names from the json file
-var maleFirstNames = [];
-var femaleFirstNames = [];
-var lastNames = []
-function collectNames(){
+const maleFirstNames = [];
+const femaleFirstNames = [];
+const lastNames = []
+function collectNames() {
     fetch('../json/firstNames.json')
-    .then(response => response.json())
-    .then(data => {
-        let maleFirstNames = data.maleFirstNames;
-        let femaleFirstNames = data.femaleFirstNames;
-    })
-    .catch(error => {
-        console.error('Error fetching JSON:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+            const male = data.maleFirstNames;
+            const female = data.femaleFirstNames;
+            male.forEach(element => {
+                maleFirstNames.push(element)
+
+            });
+
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+        });
     fetch('../json/lastNames.json')
-    .then(response => response.json())
-    .then(data => {
-        
-        let lastNames = data.lastNames;
-        console.log(lastNames);
-    })
-    .catch(error => {
-        console.error('Error fetching JSON:', error);
-    });
+        .then(response => response.json())
+        .then(data => {
+
+            let lastNames = data.lastNames;
+        })
+        .catch(error => {
+            console.error('Error fetching JSON:', error);
+        });
 }
+
 collectNames();
-function getRandomName(name){
+
+function getRandomName(name) {
     const rand = Math.floor(Math.random() * name.length);
     const randomName = name[rand];
     return randomName
 }
 
-function createName(name1, name2){
+function createName(name1) {
     const first = getRandomName(name1);
-    console.log(first)
-    do{
-        var second = getRandomName(name2);
-        console.log(second)
+    return first;
+
+
+}
+
+
+let saveName = document.getElementById("saveName");
+saveName.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const gender = document.querySelector('input[name=gender]:checked')
+    const genderType = gender['id']
+    console.log(genderType)
+    const names = [];
+    const checkedNames = document.getElementsByName('names');
+    for (let i = 0; i < checkedNames.length; i++) {
+        if (checkedNames[i].checked) {
+            names.push(checkedNames[i].id);
+        }
+
     }
-    while(first == second)
-    console.log(first,second);
-}
+    let final = '';
+    console.log(names)
+    if (genderType == 'male') {
+        if ((names.includes('firstName') || names.includes('middleName')) && (names.length == 1)) {
+            let first = createName(maleFirstNames);
+            final = first
+            
 
-function addToStorage(){
-    
-}
-
-createName(firstNames, firstNames)
-
+        }
+        else if ((names.includes('firstName') && names.includes('middleName')) && (names.length == 2)) {
+            let first = createName(maleFirstNames);
+            console.log(first)
+            do {
+                var second = getRandomName(maleFirstNames);
+                
+            }
+            while (first == second);
+            final = first + " " + second;
+        }
+        else {
+            console.log("Nope.")
+        }
+    console.log(final);
+    }
+})
